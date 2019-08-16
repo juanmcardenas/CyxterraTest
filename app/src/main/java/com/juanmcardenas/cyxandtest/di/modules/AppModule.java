6c66c1f1f6ab36c7ea24272c8f100acd094720e2
martin-cardenas <martin.cardenas@accedo.tv>
2019-08-15 22:37:26 -0500

    CX1 initial architecture initialization

---
+++ app/src/main/java/com/juanmcardenas/cyxandtest/di/modules/AppModule.java
@@ -0,0 +1,7 @@
+package com.juanmcardenas.cyxandtest.di.modules;
+
+/**
+ * Created by Martin Cardenas on 8/15/19.
+ */
+public class AppModule {
+}
---
+++ app/src/main/java/com/juanmcardenas/cyxandtest/model/repository/MainRepository.java
@@ -0,0 +1,7 @@
+package com.juanmcardenas.cyxandtest.model.repository;
+
+/**
+ * Created by Martin Cardenas on 8/15/19.
+ */
+public class MainRepository {
+}
